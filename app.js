b8590680f115635da771c6bf347c20a82b359060
gillianzili <ziliang.guo127@gmail.com>
2024-07-15 13:03:16 +0800

    enable users post with image and name

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ import mongoose from "mongoose";
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://127.0.0.1:27017/tuiter'
-console.log(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,7 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+    userName: String,
+    image: String,
 }, { collection: 'tuits' });
 export default schema;
