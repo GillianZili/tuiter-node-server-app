cc5c4af5251b3bf5ff7b64e31229ce18caa064a0
gillianzili <ziliang.guo127@gmail.com>
2024-07-18 13:38:16 +0800

    fix frontend url

--- app.js
+++ app.js
@@ -8,29 +8,30 @@ import session from "express-session";
 import AuthController from "./controllers/users/auth-controller.js";
 
 const app = express();
-app.use(
-    session({
-        secret:"any string",
-        resave:false,
-        saveUninitialized:true,
-    })
-);
 
 const FRONTEND = process.env.FRONTEND || "http://localhost:3000";
+
+const sess = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+};
 if (process.env.FRONTEND) {
     app.set("trust proxy", 1);
     sess.cookie.secure = true;
     sess.cookie.sameSite = "none";
-  }
+}
+app.use(session(sess));
 
 app.use(
     cors({
-        credentials:true,
-        origin:FRONTEND,
+        credentials: true,
+        origin: FRONTEND,
     })
 );
 app.use(express.json());
-const port=process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://127.0.0.1:27017/tuiter'
